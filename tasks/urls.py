02690688fe2dc48566d382f2bbf0683ccf23f399
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2026-02-17 15:03:56 +0100

    Modified model, views and urls to add user login

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
+
 urlpatterns = [
-	path('', views.index, name="list"),
-	path('update_task/<str:pk>/', views.updateTask, name="update_task"),
-	path('delete_task/<str:pk>/', views.deleteTask, name="delete")
-	
+    path("", views.index, name="list"),
+    path("update/<int:pk>/", views.updateTask, name="update"),
+    path("delete/<int:pk>/", views.deleteTask, name="delete"),
+    path("accounts/signup/", views.signup, name="signup"),
 ]
